777dec7c7a0c14fcc42d866368acff21f0c2fa59
ivanrubin10 <ivanrubin10@gmail.com>
2024-02-26 20:19:13 -0300

    (feature): app router with transition and responsive navbar component

---
+++ src/views/CarrierView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Carrier</h1>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>
+    Home
+  </h1>
 </template>
