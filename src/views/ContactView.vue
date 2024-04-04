9ebcc56934f6374d80c1e1658df32f0834f26ab5
ivanrubin10 <ivanrubin10@gmail.com>
2024-04-03 23:56:01 -0300

    (feature): visuals completed + basic functionality working

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,45 +1,75 @@
 <script setup lang="ts">
 import VHeroBlock from '@/components/VHeroBlock.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
+import emailjs from '@emailjs/browser';
+import { ref } from 'vue';
+const form = ref();
+function sendEmail() {
+    emailjs
+        .sendForm('service_8xwe4qx', 'template_ihvvut6', form.value, {
+            publicKey: '4Z7cO4rsSonIuZlJt',
+        })
+        .then(
+            () => {
+                console.log('SUCCESS!');
+            },
+            (error) => {
+                console.log('FAILED...', error.text);
+            },
+        );
+}
 </script>
 
 <template>
-    <VHeroBlock 
-        imageUrl="./src/assets/hero_contact.png"
-        title="Contact Us"
-    />
+    <VHeroBlock imageUrl="./src/assets/hero_contact.png" title="Contact Us" />
     <div class="overlap-box">
-        <form action="#" method="post" class="form">
+        <div class="form-title">
+            <p>Request </p>
+            <div class="form-title-quote">
+                <p>a quote</p>
+            </div>
+        </div>
+        <div class="form-title-underline"></div>
+        <form method="post" class="form" ref="form" @submit.prevent="sendEmail">
             <div class="input-row">
                 <div>
-                    <label for="name">Name:</label>
+                    <label for="name">Name</label>
                     <input type="text" id="name" name="name" required>
                 </div>
                 <div>
-                    <label for="email">Email:</label>
+                    <label for="email">Email</label>
                     <input type="email" id="email" name="email" required>
                 </div>
             </div>
             <div class="input-row">
                 <div>
-                    <label for="phone">Phone:</label>
+                    <label for="phone">Phone</label>
                     <input type="tel" id="phone" name="phone" required>
                 </div>
                 <div>
-                    <label for="subject">Subject:</label>
+                    <label for="subject">Subject</label>
                     <input type="text" id="subject" name="subject" required>
                 </div>
             </div>
             <div class="input-row">
                 <div>
-                    <label for="message">Message:</label>
+                    <label for="message">Message</label>
                     <textarea id="message" name="message" rows="5" required></textarea><br>
                 </div>
             </div>
             <div class="form-footer">
-                <input type="submit" value="Send">
-                <div class="pr-text-footer"><FontAwesomeIcon :icon="faPhone" class="pr-icon-footer"/>+1 303 915-4272</div>
+                <div class="submit-container">
+                    <input type="submit" value="Send">
+                    <span class="icon-container">
+                        <FontAwesomeIcon :icon="faArrowRight" class="pr-icon-arrow" />
+                    </span>
+                </div>
+                <div class="pr-text-footer">
+                    <FontAwesomeIcon :icon="faPhone" class="pr-icon-footer" /><a href="tel:+13039154272">+1 303
+                        915-4272</a>
+                </div>
             </div>
         </form>
     </div>
@@ -63,6 +93,7 @@ import { faPhone } from '@fortawesome/free-solid-svg-icons'
     flex-direction: column;
     align-items: center;
     padding: 10em;
+    padding-top: 3em;
     color: gray;
 }
 
@@ -78,26 +109,32 @@ import { faPhone } from '@fortawesome/free-solid-svg-icons'
 }
 
 label {
+    font-family: MundialLight;
     display: block;
     margin-bottom: 0.5em;
     text-align: start;
 }
 
-input, textarea {
+input,
+textarea {
     margin-bottom: 1em;
     padding: 0.5em;
     border-radius: 5px;
     border: 1px solid #ccc;
     width: 100%;
+    resize: none;
 }
 
 input[type="submit"] {
     padding: 0.5em 1em;
-    background-color: #007bff;
+    padding-left: 2em;
+    background-color: #1D2344;
+    font-family: AkiraExpandedDemo;
     color: white;
     border: none;
     width: 10em;
-    border-radius: 5px;
+    text-align: left;
+    border-radius: 0px;
     cursor: pointer;
 }
 
@@ -107,12 +144,46 @@ input[type="submit"] {
     justify-content: space-between;
 }
 
-.pr-icon-footer{
-  color: var(--color-light-blue-lighter);
-  margin-right: 1em;
+.pr-icon-footer {
+    color: var(--color-light-blue-lighter);
+    margin-right: 1em;
+}
+
+.pr-icon-arrow {
+    transform: translate(-45px, 3px);
+    color: white;
+    font-size: 1.5em;
 }
 
 .pr-text-footer {
     color: rgb(82, 81, 81);
+}
+
+.form-title {
+    font-family: AkiraExpandedDemo;
+    color: var(--color-indigo-100);
+    display: flex;
+    flex-direction: row;
+    gap: 1em;
+    margin-left: 10em;
+    padding-top: 4em;
+    width: max-content;
+}
+
+.form-title p {
+    margin: 0;
+    font-size: 25px;
+}
+
+.form-title-underline {
+    border-bottom: 3px solid var(--color-light-blue);
+    margin-left: 10em;
+    margin-top: 1em;
+    width: 4%;
+}
+
+.form-title-quote {
+    font-family: AkiraExpandedDemo;
+    color: var(--color-light-blue);
 }
 </style>
